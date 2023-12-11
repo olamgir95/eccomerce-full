22d1c2219d859dd44866cc1cde48d7cf1860f89e
Olamgir <ollomurodovolamgir233@gmail.com>
2023-12-11 19:00:14 +0900

    feat: prepared for project

---
+++ expo-env.d.ts
@@ -0,0 +1,7 @@
+declare module "*.png";
+declare module "*.svg";
+declare module "*.jpeg";
+declare module "*.jpg";
+declare module "redux-persist/integration/react";
+declare module "some-untyped-module";
+/// <reference types="react-scripts" />
