a98ef3cc4960c88fdf2173198f9b8bd3456dbcc8
Olamgir <ollomurodovolamgir233@gmail.com>
2024-01-12 19:16:17 +0900

    feat: develop sign up page publishing

---
+++ src/pages/Account/SignUp.tsx
@@ -0,0 +1,325 @@
+import React, { ChangeEvent, MouseEvent, useState } from "react";
+import { BsCheckCircleFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import logoLight from "../../assets/images/logo.png";
+import { TextField } from "@mui/material";
+
+const SignUp = () => {
+  // ============= Initial State Start here =============
+  const [clientName, setClientName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [checked, setChecked] = useState(false);
+  // ============= Initial State End here ===============
+  // ============= Error Msg Start here =================
+  const [errClientName, setErrClientName] = useState("");
+  const [errEmail, setErrEmail] = useState("");
+  const [errPhone, setErrPhone] = useState("");
+  const [errPassword, setErrPassword] = useState("");
+  const [errConPassword, setErrConPassword] = useState("");
+  // ============= Error Msg End here ===================
+  const [successMsg, setSuccessMsg] = useState("");
+  // ============= Event Handler Start here =============
+  const handleName = (e: ChangeEvent<HTMLInputElement>) => {
+    setClientName(e.target.value);
+    setErrClientName("");
+  };
+  const handleEmail = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+    setErrEmail("");
+  };
+  const handlePhone = (e: ChangeEvent<HTMLInputElement>) => {
+    setPhone(e.target.value);
+    setErrPhone("");
+  };
+  const handlePassword = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+    setErrPassword("");
+  };
+
+  const handleConfirmPassword = (e: ChangeEvent<HTMLInputElement>) => {
+    setConfirmPassword(e.target.value);
+    setConfirmPassword("");
+  };
+
+  // ============= Event Handler End here ===============
+  // ================= Email Validation start here =============
+  const EmailValidation = (email: string) => {
+    return String(email)
+      .toLowerCase()
+      .match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i);
+  };
+  // ================= Email Validation End here ===============
+
+  const handleSignUp = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    if (checked) {
+      if (!clientName) {
+        setErrClientName("Enter your name");
+      }
+      if (!email) {
+        setErrEmail("Enter your email");
+      } else {
+        if (!EmailValidation(email)) {
+          setErrEmail("Enter a Valid email");
+        }
+      }
+      if (!phone) {
+        setErrPhone("Enter your phone number");
+      }
+      if (!password) {
+        setErrPassword("Create a password");
+      } else {
+        if (password.length < 6) {
+          setErrPassword("Passwords must be at least 6 characters");
+        }
+      }
+
+      if (!confirmPassword) {
+        setErrConPassword("Enter your confirm password");
+      }
+
+      if (confirmPassword !== password) {
+        setErrConPassword("Please correct password");
+      }
+
+      // ============== Getting the value ==============
+      if (
+        clientName &&
+        email &&
+        EmailValidation(email) &&
+        password &&
+        password.length >= 6 &&
+        confirmPassword
+      ) {
+        setSuccessMsg(
+          `Hello dear ${clientName}, Welcome you to Shopsy Admin panel. We received your Sign up request. We are processing to validate your access. Till then stay connected and additional assistance will be sent to you by your mail at ${email}`
+        );
+        setClientName("");
+        setEmail("");
+        setPhone("");
+        setPassword("");
+        setConfirmPassword("");
+      }
+    }
+  };
+  return (
+    <div className="w-full h-screen flex items-center justify-start ">
+      <div className="w-1/2 hidden lgl:inline-flex h-full text-white">
+        <div className="w-[450px] h-full bg-primeColor px-10 flex flex-col gap-6 justify-center">
+          <Link to="/">
+            <h1 className="sm:text-3xl text-xl font-bold sm:text-left text-justify mb-3 flex items-center gap-3">
+              <img src={logoLight} alt="" className="max-w-[50px]" />
+              Shopsy
+            </h1>
+          </Link>
+          <div className="flex flex-col gap-1 -mt-1">
+            <h1 className="font-titleFont text-xl font-medium">
+              Get started for free
+            </h1>
+            <p className="text-base">Create your account to access more</p>
+          </div>
+          <div className="w-[300px] flex items-start gap-3">
+            <span className="text-green-500 mt-1">
+              <BsCheckCircleFill />
+            </span>
+            <p className="text-base text-gray-300">
+              <span className="text-white font-semibold font-titleFont">
+                Get started fast with Shopsy
+              </span>
+              <br />
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab omnis
+              nisi dolor recusandae consectetur!
+            </p>
+          </div>
+          <div className="w-[300px] flex items-start gap-3">
+            <span className="text-green-500 mt-1">
+              <BsCheckCircleFill />
+            </span>
+            <p className="text-base text-gray-300">
+              <span className="text-white font-semibold font-titleFont">
+                Access all Shopsy services
+              </span>
+              <br />
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab omnis
+              nisi dolor recusandae consectetur!
+            </p>
+          </div>
+          <div className="w-[300px] flex items-start gap-3">
+            <span className="text-green-500 mt-1">
+              <BsCheckCircleFill />
+            </span>
+            <p className="text-base text-gray-300">
+              <span className="text-white font-semibold font-titleFont">
+                Trusted by online Shoppers
+              </span>
+              <br />
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab omnis
+              nisi dolor recusandae consectetur!
+            </p>
+          </div>
+          <div className="flex items-center justify-between mt-10">
+            <p className="text-sm font-titleFont font-semibold text-gray-300 hover:text-white cursor-pointer duration-300">
+              © Shopsy
+            </p>
+            <p className="text-sm font-titleFont font-semibold text-gray-300 hover:text-white cursor-pointer duration-300">
+              Terms
+            </p>
+            <p className="text-sm font-titleFont font-semibold text-gray-300 hover:text-white cursor-pointer duration-300">
+              Privacy
+            </p>
+            <p className="text-sm font-titleFont font-semibold text-gray-300 hover:text-white cursor-pointer duration-300">
+              Security
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="w-full lgl:w-[500px] h-full flex flex-col justify-center">
+        {successMsg ? (
+          <div className="w-[500px]">
+            <p className="w-full px-4 py-10 text-green-500 font-medium font-titleFont">
+              {successMsg}
+            </p>
+            <Link to="/signin">
+              <button
+                className="w-full h-10 bg-primeColor rounded-md text-gray-200 text-base font-titleFont font-semibold 
+            tracking-wide hover:bg-black hover:text-white duration-300"
+              >
+                Sign in
+              </button>
+            </Link>
+          </div>
+        ) : (
+          <form className="w-full mt-32 lgl:w-[500px] h-screen flex items-center justify-center">
+            <div className="px-6 py-4 w-full h-[96%] flex flex-col justify-start overflow-y-scroll scrollbar-thin scrollbar-thumb-primeColor">
+              <h1 className="font-titleFont underline underline-offset-4 decoration-[1px] font-semibold text-2xl mdl:text-3xl mb-4">
+                Create your account
+              </h1>
+              <div className="flex flex-col gap-3">
+                {/* client name */}
+                <div className="flex flex-col gap-.5">
+                  <TextField
+                    id="outlined-basic"
+                    label="Name"
+                    variant="outlined"
+                    value={clientName}
+                    onChange={handleName}
+                  />
+                  {errClientName && (
+                    <p className="text-sm text-red-500 font-titleFont font-semibold px-4">
+                      <span className="font-bold italic mr-1">!</span>
+                      {errClientName}
+                    </p>
+                  )}
+                </div>
+                {/* Email */}
+                <div className="flex flex-col gap-.5">
+                  <TextField
+                    id="outlined-password-input"
+                    label="Email"
+                    type="email"
+                    autoComplete="current-email"
+                    onChange={handleEmail}
+                    value={email}
+                  />
+                  {errEmail && (
+                    <p className="text-sm text-red-500 font-titleFont font-semibold px-4">
+                      <span className="font-bold italic mr-1">!</span>
+                      {errEmail}
+                    </p>
+                  )}
+                </div>
+                {/* Phone Number */}
+                <div className="flex flex-col gap-.5">
+                  <TextField
+                    id="outlined-password-input"
+                    label="Phone Number"
+                    type="phone"
+                    onChange={handlePhone}
+                    value={phone}
+                  />
+                  {errPhone && (
+                    <p className="text-sm text-red-500 font-titleFont font-semibold px-4">
+                      <span className="font-bold italic mr-1">!</span>
+                      {errPhone}
+                    </p>
+                  )}
+                </div>
+                {/* Password */}
+                <div className="flex flex-col gap-.5">
+                  <TextField
+                    id="outlined-password-input"
+                    label="Password"
+                    type="password"
+                    autoComplete="current-password"
+                    onChange={handlePassword}
+                    value={password}
+                  />
+                  {errPassword && (
+                    <p className="text-sm text-red-500 font-titleFont font-semibold px-4">
+                      <span className="font-bold italic mr-1">!</span>
+                      {errPassword}
+                    </p>
+                  )}
+                </div>
+
+                {/* ConfirmPassword */}
+                <div className="flex flex-col gap-.5">
+                  <TextField
+                    id="outlined-password-input"
+                    label="Confirm Password"
+                    type="password"
+                    autoComplete="current-password"
+                    onChange={handleConfirmPassword}
+                    value={confirmPassword}
+                  />
+                  {errConPassword && (
+                    <p className="text-sm text-red-500 font-titleFont font-semibold px-4">
+                      <span className="font-bold italic mr-1">!</span>
+                      {errConPassword}
+                    </p>
+                  )}
+                </div>
+                {/* Checkbox */}
+                <div className="flex items-start mdl:items-center gap-2">
+                  <input
+                    onChange={() => setChecked(!checked)}
+                    className="w-4 h-4 mt-1 mdl:mt-0 cursor-pointer"
+                    type="checkbox"
+                  />
+                  <p className="text-sm text-primeColor">
+                    I agree to the Shopsy{" "}
+                    <span className="text-blue-500">Terms of Service </span>and{" "}
+                    <span className="text-blue-500">Privacy Policy</span>.
+                  </p>
+                </div>
+                <button
+                  onClick={handleSignUp}
+                  className={`${
+                    checked
+                      ? "bg-blue-500 hover:bg-blue-700   cursor-pointer"
+                      : " bg-blue-300 hover:bg-blue-400  cursor-none"
+                  } w-full  text-base font-medium h-10 rounded-md text-white duration-300`}
+                >
+                  Create Account
+                </button>
+                <p className="text-sm text-center font-titleFont font-medium">
+                  Don't have an Account?{" "}
+                  <Link to="/signin">
+                    <span className="ml-2 text-blue-400 hover:text-blue-600 duration-300">
+                      Sign in
+                    </span>
+                  </Link>
+                </p>
+              </div>
+            </div>
+          </form>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
