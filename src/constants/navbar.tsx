9198a49b0b6103df30df10a677f13f92b2aff3f3
Olamgir <ollomurodovolamgir233@gmail.com>
2024-01-16 23:43:37 +0900

    fix: modify navbarlist

---
+++ src/constants/navbar.tsx
@@ -0,0 +1,39 @@
+import { NavBarItem } from "../types/navbar";
+
+export const navBarList: NavBarItem[] = [
+  {
+    _id: 1001,
+    title: "Home",
+    link: "/",
+  },
+  {
+    _id: 1002,
+    title: "Shop",
+    link: "/shop",
+  },
+  {
+    _id: 1003,
+    title: "Community",
+    link: "/community",
+  },
+  {
+    _id: 1004,
+    title: "My page",
+    link: "/member-page",
+  },
+  {
+    _id: 1005,
+    title: "Help",
+    link: "/help",
+  },
+  {
+    _id: 1005,
+    title: "Sign In",
+    link: "/signin",
+  },
+  {
+    _id: 1005,
+    title: "Sign Up",
+    link: "/signup",
+  },
+];
