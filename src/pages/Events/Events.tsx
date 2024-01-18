df9d4c62b826cdc385ec801b9c8763abcdf121ba
Olamgir <ollomurodovolamgir233@gmail.com>
2024-01-18 16:41:16 +0900

    fix modify event section of home page

--- src/pages/Events/Events.tsx
+++ src/pages/Events/Events.tsx
@@ -1,70 +1,21 @@
 import { Box, Container, Stack } from "@mui/material";
 import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/effect-coverflow";
-import "swiper/css/pagination";
-
-// import required modules
 import { EffectCoverflow, Pagination } from "swiper/modules";
-import { homeRetriever } from "../Home/useReduxStore";
+import { homeRetriever } from "../Home/useReduxHome";
 import { useSelector } from "react-redux";
 import { serverApi } from "../../lib/config";
 import { Event } from "../../types/event";
 import Heading from "../../components/home/Products/Heading";
+import { HiOutlinePhone } from "react-icons/hi";
+import dayjs from "dayjs";
+import { BiCalendarEvent } from "react-icons/bi";
+import { MdOutlineLocationOn } from "react-icons/md";
+import { BsPersonBadge } from "react-icons/bs";
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
 
 export default function Events() {
-  const events_list = [
-    {
-      title: "Bo'yin Foodga marhamat",
-      desc: "Yangicha Uslubda Yangicha Ta'm va Yangicha his",
-      author: "Abdurahmon Mufid",
-      date: "2023/11/16",
-      location: "Toshkent, Nurafshon ko'cha",
-      img: "/restaurant/boyinfood.png",
-    },
-    {
-      title: "Katta Chegirma endi Bellissimoda",
-      desc: "Yangicha Uslubda Yangicha Ta'm va Yangicha his",
-      author: "BellissiomoUz",
-      date: "2023/10/16",
-      location: "Toshkent, Qo'yliq",
-      img: "/restaurant/bellissimo.png",
-    },
-    {
-      title: "Bo'yin Foodga marhamat",
-      desc: "Yangicha Uslubda Yangicha Ta'm va Yangicha his",
-      author: "Abdurahmon Mufid",
-      date: "2023/11/16",
-      location: "Toshkent, Nurafshon ko'cha",
-      img: "/restaurant/boyinfood.png",
-    },
-    {
-      title: "Bo'yin Foodga marhamat",
-      desc: "Yangicha Uslubda Yangicha Ta'm va Yangicha his",
-      author: "Abdurahmon Mufid",
-      date: "2023/11/16",
-      location: "Toshkent, Nurafshon ko'cha",
-      img: "/restaurant/boyinfood.png",
-    },
-    {
-      title: "Bo'yin Foodga marhamat",
-      desc: "Yangicha Uslubda Yangicha Ta'm va Yangicha his",
-      author: "Abdurahmon Mufid",
-      date: "2023/11/16",
-      location: "Toshkent, Nurafshon ko'cha",
-      img: "/restaurant/boyinfood.png",
-    },
-    {
-      title: "Bo'yin Foodga marhamat",
-      desc: "Yangicha Uslubda Yangicha Ta'm va Yangicha his",
-      author: "Abdurahmon Mufid",
-      date: "2023/11/16",
-      location: "Toshkent, Nurafshon ko'cha",
-      img: "/restaurant/boyinfood.png",
-    },
-  ];
   const { newEvents } = useSelector(homeRetriever);
 
   return (
@@ -97,213 +48,51 @@ export default function Events() {
           >
             {newEvents.map((event: Event) => {
               const image_path = `${serverApi}/${event?.event_image}`;
+              const formattedDate = dayjs(event?.event_date).format(
+                "YYYY-MM-DD HH:mm"
+              );
+
               return (
                 <SwiperSlide
                   key={event._id}
-                  className="events_info_frame w-96 h-96"
+                  className="events_info_frame w-96 h-96 relative"
                 >
-                  {/* <div className="relative w-full h-full flex flex-col items-center"> */}
-                  {/* <div className="w-full h-full"> */}
                   <img
                     src={image_path}
-                    className="w-full h-3/4 object-cover rounded-10"
+                    className="w-full h-1/2  object-cover rounded-10"
                     alt=""
                   />
-                  {/* </div> */}
-                  <div className=" bottom-5  transform  p-5 h-1/4 bg-white shadow-lg rounded-8">
+                  <div className=" bottom-5  transform  p-2 h-[224px] rounded-b-2xl bg-white shadow-lg rounded-8">
                     <div className="w-full flex flex-row justify-between items-center">
                       <div className="flex flex-col">
                         <div className="event_title_speaker">
-                          <strong>{event.event_title}</strong>
+                          <strong className=" -mt-1">
+                            {event.event_title}
+                          </strong>
                           <div className="event_organizator">
                             <img src="/icons/speaker.svg" alt="" />
-                            <p className="spec_text_author">
+                            <p className=" text-primeColor flex items-center gap-1 mt-2">
+                              <BsPersonBadge />
                               {event?.member_data?.mb_nick}
                             </p>
                           </div>
                         </div>
-                        <p className="text_desc">{event.event_description}</p>
-                        <div className="bott_info">
-                          <div className="bott_info_main">
-                            <img src="/icons/calendar.svg" alt="" />{" "}
-                            {event.event_date}
+                        <p className="text_desc text-sm text-gray-500 mt-1">
+                          {event.event_description}
+                        </p>
+                        <div className="flex mt-2 text-sm justify-between">
+                          <div className="flex items-center  gap-1">
+                            <BiCalendarEvent />
+                            {formattedDate}
                           </div>
-                          <div className="bott_info_main">
-                            <img src="/icons/location.svg" alt="" />
-                            {event.event_location}
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  {/* </div> */}
-                </SwiperSlide>
-              );
-            })}
-            {newEvents.map((event: Event) => {
-              const image_path = `${serverApi}/${event?.event_image}`;
-              return (
-                <SwiperSlide
-                  key={event._id}
-                  className="events_info_frame w-96 h-96"
-                >
-                  <div className="relative w-full h-full flex flex-col items-center">
-                    <div className="w-full h-full">
-                      <img
-                        src={image_path}
-                        className="w-full h-full object-cover rounded-10"
-                        alt=""
-                      />
-                    </div>
-                    <div className="absolute bottom-5 left-1/2 transform -translate-x-1/2 p-10 w-90 bg-white shadow-lg rounded-8">
-                      <div className="w-full flex flex-row justify-between items-center">
-                        <div className="flex flex-col">
-                          <div className="event_title_speaker">
-                            <strong>{event.event_title}</strong>
-                            <div className="event_organizator">
-                              <img src="/icons/speaker.svg" alt="" />
-                              <p className="spec_text_author">
-                                {event?.member_data?.mb_nick}
-                              </p>
-                            </div>
-                          </div>
-                          <p className="text_desc">{event.event_description}</p>
-                          <div className="bott_info">
-                            <div className="bott_info_main">
-                              <img src="/icons/calendar.svg" alt="" />{" "}
-                              {event.event_date}
-                            </div>
-                            <div className="bott_info_main">
-                              <img src="/icons/location.svg" alt="" />
-                              {event.event_location}
-                            </div>
+                          <div className="flex items-center  gap-1">
+                            <HiOutlinePhone />
+                            {event?.member_data?.mb_phone}
                           </div>
                         </div>
-                      </div>
-                    </div>
-                  </div>
-                </SwiperSlide>
-              );
-            })}
-            {newEvents.map((event: Event) => {
-              const image_path = `${serverApi}/${event?.event_image}`;
-              return (
-                <SwiperSlide key={event._id} className="events_info_frame">
-                  <div className="relative w-full h-full flex flex-col items-center">
-                    <div className="w-full h-full">
-                      <img
-                        src={image_path}
-                        className="w-full h-full object-cover rounded-10"
-                        alt=""
-                      />
-                    </div>
-                    <div className="absolute bottom-5 left-1/2 transform -translate-x-1/2 p-10 w-90 bg-white shadow-lg rounded-8">
-                      <div className="w-full flex flex-row justify-between items-center">
-                        <div className="flex flex-col">
-                          <div className="event_title_speaker">
-                            <strong>{event.event_title}</strong>
-                            <div className="event_organizator">
-                              <img src="/icons/speaker.svg" alt="" />
-                              <p className="spec_text_author">
-                                {event?.member_data?.mb_nick}
-                              </p>
-                            </div>
-                          </div>
-                          <p className="text_desc">{event.event_description}</p>
-                          <div className="bott_info">
-                            <div className="bott_info_main">
-                              <img src="/icons/calendar.svg" alt="" />{" "}
-                              {event.event_date}
-                            </div>
-                            <div className="bott_info_main">
-                              <img src="/icons/location.svg" alt="" />
-                              {event.event_location}
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </SwiperSlide>
-              );
-            })}
-            {newEvents.map((event: Event) => {
-              const image_path = `${serverApi}/${event?.event_image}`;
-              return (
-                <SwiperSlide key={event._id} className="events_info_frame">
-                  <div className="relative w-full h-full flex flex-col items-center">
-                    <div className="w-full h-full">
-                      <img
-                        src={image_path}
-                        className="w-full h-full object-cover rounded-10"
-                        alt=""
-                      />
-                    </div>
-                    <div className="absolute bottom-5 left-1/2 transform -translate-x-1/2 p-10 w-90 bg-white shadow-lg rounded-8">
-                      <div className="w-full flex flex-row justify-between items-center">
-                        <div className="flex flex-col">
-                          <div className="event_title_speaker">
-                            <strong>{event.event_title}</strong>
-                            <div className="event_organizator">
-                              <img src="/icons/speaker.svg" alt="" />
-                              <p className="spec_text_author">
-                                {event?.member_data?.mb_nick}
-                              </p>
-                            </div>
-                          </div>
-                          <p className="text_desc">{event.event_description}</p>
-                          <div className="bott_info">
-                            <div className="bott_info_main">
-                              <img src="/icons/calendar.svg" alt="" />{" "}
-                              {event.event_date}
-                            </div>
-                            <div className="bott_info_main">
-                              <img src="/icons/location.svg" alt="" />
-                              {event.event_location}
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </SwiperSlide>
-              );
-            })}
-            {newEvents.map((event: Event) => {
-              const image_path = `${serverApi}/${event?.event_image}`;
-              return (
-                <SwiperSlide key={event._id} className="events_info_frame">
-                  <div className="relative w-full h-full flex flex-col items-center">
-                    <div className="w-full h-full">
-                      <img
-                        src={image_path}
-                        className="w-full h-full object-cover rounded-10"
-                        alt=""
-                      />
-                    </div>
-                    <div className="absolute bottom-5 left-1/2 transform -translate-x-1/2 p-10 w-90 bg-white shadow-lg rounded-8">
-                      <div className="w-full flex flex-row justify-between items-center">
-                        <div className="flex flex-col">
-                          <div className="event_title_speaker">
-                            <strong>{event.event_title}</strong>
-                            <div className="event_organizator">
-                              <img src="/icons/speaker.svg" alt="" />
-                              <p className="spec_text_author">
-                                {event?.member_data?.mb_nick}
-                              </p>
-                            </div>
-                          </div>
-                          <p className="text_desc">{event.event_description}</p>
-                          <div className="bott_info">
-                            <div className="bott_info_main">
-                              <img src="/icons/calendar.svg" alt="" />{" "}
-                              {event.event_date}
-                            </div>
-                            <div className="bott_info_main">
-                              <img src="/icons/location.svg" alt="" />
-                              {event.event_location}
-                            </div>
-                          </div>
+                        <div className="flex mt-2 items-center text-sm  gap-1">
+                          <MdOutlineLocationOn />
+                          {event.event_location}
                         </div>
                       </div>
                     </div>
